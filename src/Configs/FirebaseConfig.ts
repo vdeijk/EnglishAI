ff8a39ab1958533174205a7e329e7c3b3ead461e
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2024-12-28 09:55:19 +0100

    changes

--- src/Configs/FirebaseConfig.ts
+++ src/Configs/FirebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_N9KoDI6TWTIK2kopDFzTvyeks-irfm0",
-  authDomain: "langrocket.firebaseapp.com",
-  projectId: "langrocket",
-  storageBucket: "langrocket.firebasestorage.app",
-  messagingSenderId: "51590582942",
-  appId: "1:51590582942:web:69f18f8945a64247d9a18f",
-  measurementId: "G-LHK2LD7Y34",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Auth0Provider
-        domain={import.meta.env.VITE_AUTH0_DOMAIN}
-        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-        authorizationParams={{ redirect_uri: import.meta.env.VITE_AUTH0_REDIRECT_URI }}
-      >
-        <App />
-      </Auth0Provider>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
